071adb3fd71b70945c4222e06584797d93abfbab
EmanuelDiego0 <emanueldiego0@gmail.com>
2018-09-19 13:59:18 -0300

    modificacoes 2

--- genetico_fpca.py
+++ genetico_fpca.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 
-TESTE1: HOUDOUT=5, TAM_POP=3, TAXA=0.02, EPOCHS=100
+TESTE1: HOLDOUT=5, TAM_POP=3, TAXA=0.02, EPOCHS=100
 
 @author: Emanuel
 """
@@ -20,9 +20,9 @@ from sklearn.model_selection import train_test_split
 #constantes
 M = 644             #numero de features
 K = 70              #numero maximo de componentes
-HOLDOUT = 5        #rodadas
-TAXA = 0.02         #taxa de probabilidade de mutacao
-TAM_POP = 3        #qtde de individuos da populacao
+HOLDOUT = 10        #rodadas
+TAXA = 0.01         #taxa de probabilidade de mutacao
+TAM_POP = 10        #qtde de individuos da populacao
 EPOCHS = 100        #numero de epocas
 
 
@@ -38,31 +38,32 @@ def CarregarYaleFaces():
     Y = [f.split('.')[0] for f in files]
     return images_yale_flatten, Y
 
+
 def F_Eigenfaces(X, W, k, R):
-        r = 0.01
-        n = X.shape[0]
-        m = X.shape[1]
-        mean = np.mean(X, axis = 0)
-        D = np.zeros((n, n))
-        for j in range(m):
-            a = np.power(X[:,j], r) - np.power(X[:,j].mean(), r)
-            a = a.reshape(n,1)
-            b = a.T
-            D = D + (a * b)
-        val, vec = np.linalg.eig(D)
-        val = np.abs(val)
-        vec_c = 1. / np.power((n * val), 0.5)
-        vec_c = vec_c * (np.power(X, R) - np.power(mean, R)).T.dot(vec)
-        X_ = vec_c.T.dot((np.power(X, R) - np.power(mean, R)).T)
-        X__ = vec_c.T.dot((np.power(W, R) - np.power(mean, R)).T)
-        return X_.T[:,:k], X__.T[:,:k]
+    r = 0.01
+    n = X.shape[0]
+    m = X.shape[1]
+    mean = np.mean(X, axis = 0)
+    D = np.zeros((n, n))
+    for j in range(m):
+        a = np.power(X[:,j], r) - np.power(X[:,j].mean(), r)
+        a = a.reshape(n,1)
+        b = a.T
+        D = D + (a * b)
+    val, vec = np.linalg.eig(D)
+    val = np.abs(val)
+    vec_c = 1. / np.power((n * val), 0.5)
+    vec_c = vec_c * (np.power(X, R) - np.power(mean, R)).T.dot(vec)
+    X_ = vec_c.T.dot((np.power(X, R) - np.power(mean, R)).T)
+    X__ = vec_c.T.dot((np.power(W, R) - np.power(mean, R)).T)
+    return X_.T[:,:k], X__.T[:,:k]
 
 #ALGORITMO GENETICO
 class Individuo():
     def __init__(self, X, Y):
         self.X = X
         self.Y = Y
-        cromossomo = np.random.rand(M) / 10   #vetor de tamanho M com valores no intervalo [0,1]
+        cromossomo = np.random.rand(M) #/ 10   #vetor de tamanho M com valores no intervalo [0,1]
         self.cromossomo = cromossomo.tolist()
         self.nota = 0
     
@@ -72,7 +73,7 @@ class Individuo():
     
         n_components = 1
         
-        for i in range(10):
+        for i in range(15):
             
             acc = []
             for j in range(HOLDOUT):
@@ -95,10 +96,26 @@ class Individuo():
             else:
                 n_components += 5
         
-        #print(acc_geral)
+        
         self.nota = np.array(acc_geral).mean()
+        #print('\n'+str(self.nota))
     
     
+    def crossover2(self, outro_individuo):
+        
+        corte = random.randint(1,M)
+        
+        filho1 = Individuo(self.X, self.Y)
+        filho2 = Individuo(self.X, self.Y)
+        
+        cromossomo_filho1 = self.cromossomo[:corte] + outro_individuo.cromossomo[corte:]
+        cromossomo_filho2 = outro_individuo.cromossomo[:corte] + self.cromossomo[corte:]
+        
+        filho1.cromossomo = cromossomo_filho1
+        filho2.cromossomo = cromossomo_filho2
+        return [filho1, filho2]
+    
+        
     def crossover(self, outro_individuo):
         #M = 8
         n = 0
@@ -107,7 +124,7 @@ class Individuo():
             slice = random.randint(50, 100)
             n += slice
             slices.append(n)
-        
+            
         if(slices[-1] >= M-1):
             slices.pop(-1)
         
@@ -199,28 +216,29 @@ class AlgoritmoGenetico():
         
         self.inicializa_populacao()
         
-        for individuo in self.populacao:
-            individuo.fitness()
-        
-        self.ordena_populacao()
-        self.melhor_solucao = self.populacao[0]
-        #self.lista_solucoes.append(self.melhor_solucao.nota_avaliacao)
-        
-        #self.visualiza_geracao()
-    
         #for e in progressbar.progressbar(range(epochs)):
         for e in range(epochs):
+            #print(len(self.populacao))
             #print("loop1")
+            f = []
             for i in progressbar.progressbar(self.populacao):
             #for i in genetico.populacao:
                 i.fitness()
+                f.append(i.nota)
                 #print("loop2") 
                 #print("nota: %s" %(str(i.nota)))
+            
+            f = [round(a,2) for a in f]
+            
             self.ordena_populacao()
             self.melhor_individuo(self.populacao[0])
-            print('# Epoca: %s\tFitness Melhor Cromossomo: %s\tFitness Medio: %s' 
-                  %(str(e+1), str(self.melhor_solucao.nota), str(self.fitness_medio_populacao())))
+            
+            print('\n# Epoca: %s\tNotas: %s\tFitness Melhor Cromossomo: %s\tFitness Medio: %s' 
+                  %(str(e+1), str(f),str(round(self.melhor_solucao.nota,2)), str(round(self.fitness_medio_populacao(),2))))
             soma_fitness = self.soma_fitness()
+            
+            #ELITISMO AQUI
+            self.populacao = self.populacao[:int(self.tamanho_populacao/2)]
             
             nova_populacao = []
             for novos in range(0, self.tamanho_populacao, 2):
@@ -232,10 +250,10 @@ class AlgoritmoGenetico():
                     if(pai1 != pai2):
                         break
                 
-                filhos = self.populacao[pai1].crossover(self.populacao[pai2])
+                filhos = self.populacao[pai1].crossover2(self.populacao[pai2])
                 nova_populacao.append(filhos[0].mutacao(taxa_mutacao))
                 nova_populacao.append(filhos[1].mutacao(taxa_mutacao))
-            
+                
             self.populacao = list(nova_populacao)
             
         self.ordena_populacao()
---
+++ genetico_fpca_deap.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 10:10:33 2018
+
+@author: EMANUEL
+"""
+
